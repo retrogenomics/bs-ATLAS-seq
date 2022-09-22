c869ea35196b7cad07f34bed58e357ddd536a625
Gael Cristofari <Gael.Cristofari@unice.fr>
2022-09-22 18:49:55 +0200

    Update config_dir.sh

--- config_dir.sh
+++ config_dir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # path to root directory
-path_to_bioinfo="${HOME}/bs-ATLAS-seq"
+path_to_bioinfo="${HOME}/bs-ATLAS-seq-main"
 
 # path to software dependencies
 path_to_bwt2="/opt/local/bin"
@@ -9,12 +9,12 @@ picard="/usr/local/picard/picard.jar"
 
 # reference sequences and bismark indexes
 reference="hg38"
-path_to_reference="${path_to_bioinfo}/datasets/references/hg38"
-path_to_L1HS="${path_to_bioinfo}/datasets/references/L1/bismark-bwt2"
+path_to_reference="${path_to_bioinfo}/references/hg38"
+path_to_L1HS="${path_to_bioinfo}/references/L1"
 
 # path to annotation files
-rmsk="${path_to_bioinfo}/datasets/annotations/hg38_rmsk_L1_repPercent.bed"
-genes="${path_to_bioinfo}/datasets/annotations/hg38_NCBIRefSeq_refGene.bed"
-blacklist="${path_to_bioinfo}/datasets/annotations/hg38.encode4_unified_blacklist.ENCFF356LFX.bed"
+rmsk="${path_to_bioinfo}/annotations/hg38_rmsk_L1_repPercent.bed"
+genes="${path_to_bioinfo}/annotations/hg38_NCBIRefSeq_refGene.bed"
+blacklist="${path_to_bioinfo}/annotations/hg38.encode4_unified_blacklist.ENCFF356LFX.bed"
 
 echo -e "Default directories loaded."
